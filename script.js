980fe9c8a988fd9d2987c7f313bc898072ade4f3
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-15 11:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+  
+  
+document.getElementById("Login").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const firstName = document.getElementsByName("First Name")[0].value;
+  const lastName = document.getElementsByName("Last Name")[0].value;
+  const phoneNumber = document.getElementsByName("Phone Number")[0].value;
+  const emailId = document.getElementsByName("Email ID")[0].value;
+
+  const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+  alert(message);
+});
